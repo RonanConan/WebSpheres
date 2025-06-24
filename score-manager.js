6d013b8f80949a17919ec1b993098928443856a1
RonanConan <104478015+RonanConan@users.noreply.github.com>
2025-06-24 12:26:47 +0100

    Update score-manager.js

--- score-manager.js
+++ score-manager.js
@@ -1,11 +1,17 @@
 AFRAME.registerComponent('score-manager', {
     init: function() {
         this.score = 0;
+        this.progressDisplay = document.querySelector('#progress-display');
     },
     
     addPoint: function() {
         this.score++;
         this.updateDisplay();
+    },
+    
+    updateProgress: function(current, total) {
+        const percentage = Math.round((current / total) * 100);
+        this.progressDisplay.setAttribute('value', `Progress: ${percentage}%`);
     },
     
     updateDisplay: function() {
