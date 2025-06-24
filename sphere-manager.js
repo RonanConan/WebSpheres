a795819f968ea6e511fc2d4ddb9d3a58ce2ab884
RonanConan <104478015+RonanConan@users.noreply.github.com>
2025-06-24 13:05:50 +0100

    Update sphere-manager.js

--- sphere-manager.js
+++ sphere-manager.js
@@ -11,50 +11,6 @@ AFRAME.registerComponent('sphere-manager', {
     },
     
     createSpheres: function() {
-        const radius = 0.7;
-        const height = 1.2;
-        
-        for (let i = 0; i < 11; i++) {
-            let angle = -90 + (i * 18);
-            let x = radius * Math.sin(angle * Math.PI / 180);
-            let z = -radius * Math.cos(angle * Math.PI / 180);
-            
-            let sphere = document.createElement('a-sphere');
-            sphere.setAttribute('position', `${x} ${height} ${z}`);
-            sphere.setAttribute('color', '#ff0000');
-            sphere.setAttribute('radius', '0.025');
-            sphere.setAttribute('visible', 'false');
-            sphere.setAttribute('id', `sphere-${i}`);
-            
-            this.el.sceneEl.appendChild(sphere);
-            this.allSpheres.push(sphere);
-        }
-    },
-    
-    createFloatingScore: function() {
-        this.floatingScore = document.createElement('a-text');
-        this.floatingScore.setAttribute('value', '+1');
-        this.floatingScore.setAttribute('color', '#ffff00');
-        this.floatingScore.setAttribute('align', 'center');
-        this.floatingScore.setAttribute('visible', 'false');
-        this.floatingScore.setAttribute('scale', '0.3 0.3 0.3');
-        this.el.sceneEl.appendChild(this.floatingScore);
-    },
-    
-    showFloatingScore: function(spherePosition) {
-        const floatingPos = {
-            x: spherePosition.x,
-            y: spherePosition.y + 0.08,
-            z: spherePosition.z
-        };
-        
-        this.floatingScore.setAttribute('position', `${floatingPos.x} ${floatingPos.y} ${floatingPos.z}`);
-        this.floatingScore.setAttribute('visible', 'true');
-        
-        setTimeout(() => {
-            this.floatingScore.setAttribute('visible', 'false');
-        }, 300);
-    },
         const radius = 2;
         const height = 1.5;
         
@@ -169,7 +125,7 @@ AFRAME.registerComponent('sphere-manager', {
     },
     
     isInsideSphere: function(controllerPos, spherePos) {
-        const size = 0.05;
+        const size = 0.2;
         return Math.abs(controllerPos.x - spherePos.x) < size &&
                Math.abs(controllerPos.y - spherePos.y) < size &&
                Math.abs(controllerPos.z - spherePos.z) < size;
