8cb85e5a632060b9ae5b7bff063211dc9bad7d67
RonanConan <104478015+RonanConan@users.noreply.github.com>
2025-07-25 14:15:58 +0100

    Update audio-manager.js

--- audio-manager.js
+++ audio-manager.js
@@ -11,6 +11,30 @@ AFRAME.registerComponent('audio-manager', {
         this.criticalSound = document.createElement('audio');
         this.criticalSound.src = 'sounds/critical-hit.mp3';
         this.criticalSound.preload = 'auto';
+        
+        this.reachCalibrationSound = document.createElement('audio');
+        this.reachCalibrationSound.src = 'sounds/reach-calibration.mp3';
+        this.reachCalibrationSound.preload = 'auto';
+        
+        this.heightCalibrationSound = document.createElement('audio');
+        this.heightCalibrationSound.src = 'sounds/height-calibration.mp3';
+        this.heightCalibrationSound.preload = 'auto';
+        
+        this.lapCalibrationSound = document.createElement('audio');
+        this.lapCalibrationSound.src = 'sounds/lap-calibration.mp3';
+        this.lapCalibrationSound.preload = 'auto';
+        
+        this.switchSound = document.createElement('audio');
+        this.switchSound.src = 'sounds/switch.mp3';
+        this.switchSound.preload = 'auto';
+        
+        this.leftHandSound = document.createElement('audio');
+        this.leftHandSound.src = 'sounds/left-hand.mp3';
+        this.leftHandSound.preload = 'auto';
+        
+        this.rightHandSound = document.createElement('audio');
+        this.rightHandSound.src = 'sounds/right-hand.mp3';
+        this.rightHandSound.preload = 'auto';
     },
     
     playHitSound: function(hitType) {
@@ -20,6 +44,28 @@ AFRAME.registerComponent('audio-manager', {
         } else if (hitType === 'critical') {
             this.criticalSound.currentTime = 0;
             this.criticalSound.play();
+        }
+    },
+    
+    playCalibrationSound: function(soundType) {
+        if (soundType === 'reach-calibration') {
+            this.reachCalibrationSound.currentTime = 0;
+            this.reachCalibrationSound.play();
+        } else if (soundType === 'height-calibration') {
+            this.heightCalibrationSound.currentTime = 0;
+            this.heightCalibrationSound.play();
+        } else if (soundType === 'lap-calibration') {
+            this.lapCalibrationSound.currentTime = 0;
+            this.lapCalibrationSound.play();
+        } else if (soundType === 'switch') {
+            this.switchSound.currentTime = 0;
+            this.switchSound.play();
+        } else if (soundType === 'left-hand') {
+            this.leftHandSound.currentTime = 0;
+            this.leftHandSound.play();
+        } else if (soundType === 'right-hand') {
+            this.rightHandSound.currentTime = 0;
+            this.rightHandSound.play();
         }
     }
 });
